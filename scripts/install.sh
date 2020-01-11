81259881fee29aa69bd5d557bd4339fe630f3118
Oracle Public Cloud User <opc@instance1.subnet.vcn.oraclevcn.com>
2020-01-11 07:16:32 +0000

    add install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,19 @@
+# to install:
+# download this file
+# chmod +x install.sh
+# ./install.sh
+cd $HOME
+git clone --bare https://github.com/shenghsi/cfg.git $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
