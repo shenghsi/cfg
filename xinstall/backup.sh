12173b97a9c8237b232508c41a46de777ef9a396
Sheng Xi <shenghsi@gmail.com>
2020-06-06 22:37:17 -0700

    only app names

--- xinstall/backup.sh
+++ xinstall/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 brew cask list  > brewcasklist
 brew list > brewlist
-ls -l /Applications > macApplist
+ls -l /Applications | sed -E 's/.*[0-9]+ //' > macApplist
 pip3 list > pip3list
 pip list > piplist
