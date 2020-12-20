63b51a1e4b918d18f6f7a0d489a927046f6bcce8
Sheng Xi <shenghsi@gmail.com>
2020-12-19 17:18:55 -0800

    use cfg as command name

--- xinstall/install.sh
+++ xinstall/install.sh
@@ -4,16 +4,17 @@
 # ./install.sh
 cd $HOME
 git clone --bare https://github.com/shenghsi/cfg.git $HOME/.cfg
-function config {
+# 2020-12-19, rename config to cfg
+function cfg {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 mkdir -p .config-backup
-config checkout
+cfg checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    cfg checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
-config checkout
-config config status.showUntrackedFiles no
+cfg checkout
+cfg config status.showUntrackedFiles no
